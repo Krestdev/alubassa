2ff56a8989694423ab5c7ed42b1e6dea43394a3a
socrateNz <sssnzogning0@gmail.com>
2025-08-08 09:25:03 +0100

    header position sticky

--- components/header.tsx
+++ components/header.tsx
@@ -46,49 +46,52 @@ const Header = () => {
     ]
 
     return (
-        <div className='flex items-center justify-between containerBloc h-[88px]'>
-            <div className='flex flex-row items-center gap-4'>
-                <Link href={"/"}><img src="/logo.png" alt="logo" className='h-6 w-auto' /></Link>
-                <span className='hidden lg:flex gap-2'>
-                    {
-                        pages.map((page, id) => (
-                            <Navlink key={id} title={page.title} href={page.url} />
-                        ))
-                    }
+        <div className='w-full sticky top-0 z-50'>
+            <div className='flex items-center justify-between containerBloc h-[88px]'>
+                <div className='absolute w-full h-full top-0 left-0 -z-10 bg-white' />
+                <div className='flex flex-row items-center gap-4'>
+                    <Link href={"/"}><img src="/logo.png" alt="logo" className='h-6 w-auto' /></Link>
+                    <span className='hidden lg:flex gap-2'>
+                        {
+                            pages.map((page, id) => (
+                                <Navlink key={id} title={page.title} href={page.url} />
+                            ))
+                        }
+                    </span>
+                </div>
+                <div className='hidden lg:flex flex-row items-center gap-3'>
+                    <LocaleSwitcher />
+                    <Link href={"/contact"}><Button variant={"default"}>{t("contact")}</Button></Link>
+                </div>
+                <span className='inline-block lg:hidden'>
+                    <MenuToggle isOpen={menuToggle} toggle={() => { setMenuToggle(prev => !prev) }} />
                 </span>
-            </div>
-            <div className='hidden lg:flex flex-row items-center gap-3'>
-                <LocaleSwitcher />
-                <Link href={"/contact"}><Button variant={"default"}>{t("contact")}</Button></Link>
-            </div>
-            <span className='inline-block lg:hidden'>
-                <MenuToggle isOpen={menuToggle} toggle={() => { setMenuToggle(prev => !prev) }} />
-            </span>
 
-            <Drawer open={menuToggle} onOpenChange={setMenuToggle} direction='right'>
-                <DrawerContent className='flex flex-col gap-5 bg-primary max-w-[200px]'>
-                    <div className="mx-auto w-full max-w-sm flex flex-col gap-4">
-                        <DrawerTitle className='flex items-center justify-between'>
-                            <MenuToggle color='#FFFFFF' isOpen={menuToggle} toggle={() => { setMenuToggle(prev => !prev) }} />
-                            <LocaleSwitcher className='text-white' />
-                        </DrawerTitle>
+                <Drawer open={menuToggle} onOpenChange={setMenuToggle} direction='right'>
+                    <DrawerContent className='flex flex-col gap-5 bg-primary max-w-[200px]'>
+                        <div className="mx-auto w-full max-w-sm flex flex-col gap-4">
+                            <DrawerTitle className='flex items-center justify-between'>
+                                <MenuToggle color='#FFFFFF' isOpen={menuToggle} toggle={() => { setMenuToggle(prev => !prev) }} />
+                                <LocaleSwitcher className='text-white' />
+                            </DrawerTitle>
 
-                        <div className='flex flex-col gap-2'>
-                            {
-                                pages.map((page, id) => (
-                                    <Button onClick={() => { setMenuToggle(false) }} key={id} variant={"link"} className='items-end w-fit'>
-                                        <Navlink className="text-white" key={id} title={page.title} href={page.url} />
-                                    </Button>
-                                ))
+                            <div className='flex flex-col gap-2'>
+                                {
+                                    pages.map((page, id) => (
+                                        <Button onClick={() => { setMenuToggle(false) }} key={id} variant={"link"} className='items-end w-fit'>
+                                            <Navlink className="text-white" key={id} title={page.title} href={page.url} />
+                                        </Button>
+                                    ))
 
-                            }
-                            <Button onClick={() => { setMenuToggle(false) }} variant={"link"} className='items-end w-fit'>
-                                <Navlink className="text-white" title={t("contact")} href={"/contact"} />
-                            </Button>
+                                }
+                                <Button onClick={() => { setMenuToggle(false) }} variant={"link"} className='items-end w-fit'>
+                                    <Navlink className="text-white" title={t("contact")} href={"/contact"} />
+                                </Button>
+                            </div>
                         </div>
-                    </div>
-                </DrawerContent>
-            </Drawer>
+                    </DrawerContent>
+                </Drawer>
+            </div>
         </div>
     )
 }
